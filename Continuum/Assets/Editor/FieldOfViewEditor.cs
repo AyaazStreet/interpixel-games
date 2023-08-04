e32965bdcb270ed345c7353ac84dea069cb38dd3
TheMythik <mark@dreamwings.co.za>
2023-08-04 10:59:01 +0200

    Laying groundwork for new FOV implimentation
    Also added editor visualization for FOV

---
+++ Continuum/Assets/Editor/FieldOfViewEditor.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(EnemyFOV))]
+public class FieldOfViewEditor : Editor
+{
+    private void OnSceneGUI()
+    {
+        EnemyFOV fov = (EnemyFOV)target;
+        Handles.color = Color.white;
+
+        //Draw Circle
+        Handles.DrawWireArc(fov.transform.position, Vector3.forward, Vector3.up, 360, fov.viewRadius);
+        
+        //Draw angle lines
+        Vector3 viewAngleA = fov.DirFromAngle(-fov.viewAngle / 2, false);
+        Vector3 viewAngleB = fov.DirFromAngle(fov.viewAngle / 2, false);
+        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleA * fov.viewRadius);
+        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleB * fov.viewRadius);
+    }
+}
--- Continuum/Assets/Scripts/Enemy/EnemyFOV.cs
+++ Continuum/Assets/Scripts/Enemy/EnemyFOV.cs
@@ -4,33 +4,18 @@ using UnityEngine;
 
 public class EnemyFOV : MonoBehaviour
 {
-    public float distance = 6.6f;
-    public float angle = 45f;
-    public int segNum = 12;
+    public float viewRadius;
 
-    PolygonCollider2D col;
-    
-    void Start()
-    {
-        col = GetComponent<PolygonCollider2D>();
-        SetCollider();
-    }
+    [Range(0,360)]
+    public float viewAngle;
 
-    void Update()
+    public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
     {
-        SetCollider();
-    }
-
-    void SetCollider()
-    {
-        //idek whats going on here #spaghetti code
-        Vector2[] newPoints = new Vector2[segNum + 2];
-        newPoints[0] = Vector2.zero;
-        newPoints[1] = Quaternion.Euler(0, 0, -angle) * Vector2.up * distance; ;
-        for (int i = 2; i < segNum + 2; i++)
+        if (!angleIsGlobal)
         {
-            newPoints[i] = Quaternion.Euler(0, 0, angle * 2 / segNum) * newPoints[i-1];
+            angleInDegrees -= transform.eulerAngles.z;
         }
-        col.points = newPoints;
+
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), Mathf.Cos(angleInDegrees * Mathf.Deg2Rad), 0);
     }
 }
