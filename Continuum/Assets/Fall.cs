348d4436230bfd99d8c643595a4f2a369acde532
TheMythik <mark@dreamwings.co.za>
2023-08-05 23:45:56 +0200

    Falling scripts added

---
+++ Continuum/Assets/Fall.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Cinemachine.CinemachineTriggerAction.ActionSettings;
+
+public class Fall : MonoBehaviour
+{
+    public float globalTimescale;
+    public float? localTimescale;
+    private float timeMod;
+
+    public Transform parentTransform;
+    public Rigidbody2D parentRb;
+
+    private float fallFactor = 1f;
+    private float fallDistance = 0.3f;
+
+    private void Start()
+    {
+        //Initialise timescales
+        localTimescale = null;
+        globalTimescale = TimeScaleManager.globalTimescale;
+        timeMod = 1f;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Pits"))
+        {
+            if (parentTransform.localScale.y <= (1f - fallDistance))
+            {
+                parentTransform.localScale -= new Vector3(fallFactor * Time.deltaTime * timeMod, fallFactor * Time.deltaTime * timeMod, 0);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (parentRb.velocity.magnitude < 10f)
+        {
+            if (parentTransform.localScale.y > (1f - fallDistance))
+            {
+                parentTransform.localScale -= new Vector3(fallFactor * Time.deltaTime * timeMod, fallFactor * Time.deltaTime * timeMod, 0);
+            }
+        }
+    }
+}
